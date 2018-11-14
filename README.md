b9ebc14291b119e406ac277087287b8f82f3e511
giorgioriccardi <griccardi@my.bcit.ca>
2018-11-14 14:30:06 -0800

    clean functions.php & update instructions

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ WordPress starter child theme generator with Sass pre-processor and Gulp task ru
 Wordpress Starter child-theme sass-gulp has been tested on Mac and Windows.
 
 ### How to install `Wordpress Starter child-theme sass-gulp`:
-
 1. `$ git clone https://github.com/giorgioriccardi/wordpress-starter-child-theme-sass-gulp.git`
 2. `$ npm install`
 3. `$ gulp watch`
+4. run `$ gulp js` to minify all .js files
+5. run `$gulp images` to optimize images in the `images/src` folder 
--- functions.php
+++ functions.php
@@ -1,23 +1,6 @@
 <?php
 
 /********************************************************/
-/* Load parent theme styles
-/********************************************************/
-// add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-}
-
-/********************************************************/
-/* Load a Script from a Child Theme without Dependencies
-/********************************************************/
-// http://codex.wordpress.org/Function_Reference/wp_enqueue_script
-// add_action( 'wp_enqueue_scripts', 'ssws_assets' );
-function ssws_assets() {
-	wp_enqueue_script( 'umbrella-js', get_stylesheet_directory_uri() . '/bower_components/umbrella/umbrella.min.js' );
-}
-
-/********************************************************/
 /* Register and enqueue a script that does not depend on a JavaScript library.
 /********************************************************/
 add_action( 'wp_enqueue_scripts', 'child_add_scripts' );
@@ -32,15 +15,5 @@ function child_add_scripts() {
 
     wp_enqueue_script( 'ssws-custom-js' );
 }
-// it requires jQuery(document) instead of $(document) or better:
-// wrap your js into this function
-// jQuery(document).ready(function($) {
-//    //some js code within here
-// });
+// FYI in case you are writing some custom jQuery scripts:
 // http://codex.wordpress.org/Function_Reference/wp_enqueue_script#jQuery_noConflict_Wrappers
-
-
-
-/********************************************************/
-// .............
-/********************************************************/
