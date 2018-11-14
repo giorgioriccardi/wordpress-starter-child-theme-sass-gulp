43a4a0905d1ec380746108c1cd18d7b83ad96563
giorgioriccardi <griccardi@my.bcit.ca>
2018-11-14 13:11:09 -0800

    first sass & wp structure

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+require('es6-promise').polyfill();
+
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    // rtlcss        = require('gulp-rtlcss'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    plumber       = require('gulp-plumber'),
+    gutil         = require('gulp-util'),
+    rename        = require('gulp-rename'),
+    concat        = require('gulp-concat'),
+    jshint        = require('gulp-jshint'),
+    uglify        = require('gulp-uglify'),
+    imagemin      = require('gulp-imagemin'),
+    browserSync   = require('browser-sync').create(),
+    reload        = browserSync.reload;
+
+var onError = function( err ) {
+  console.log('An error occurred:', gutil.colors.magenta(err.message));
+  gutil.beep();
+  this.emit('end');
+};
+
+// Sass
+gulp.task('sass', function() {
+  return gulp.src('./sass/**/*.scss')
+  .pipe(plumber({ errorHandler: onError }))
+  .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./'))
+
+  // .pipe(rtlcss())                     // Convert to RTL
+  // .pipe(rename({ basename: 'rtl' }))  // Rename to rtl.css
+  // .pipe(gulp.dest('./'));             // Output RTL stylesheets (rtl.css)
+});
+
+// JavaScript
+gulp.task('js', function() {
+  return gulp.src(['./js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
+  .pipe(concat('app.js'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(uglify())
+  .pipe(gulp.dest('./js'));
+});
+
+// Images
+gulp.task('images', function() {
+  return gulp.src('./images/src/*')
+  .pipe(plumber({ errorHandler: onError }))
+  .pipe(imagemin({ optimizationLevel: 7, progressive: true }))
+  .pipe(gulp.dest('./images/dist'));
+});
+
+// Watch
+gulp.task('watch', function() {
+  // browserSync.init({
+  //   files: ['./**/*.php'],
+  //   proxy: 'http://localhost:8888/wordpress/',
+  // });
+  gulp.watch('./sass/**/*.scss', ['sass', reload]);
+  gulp.watch(['./js/*.js', '!./js/app.min.js'], ['js', reload]);
+  gulp.watch('images/src/*', ['images', reload]);
+});
+
+gulp.task('default', ['sass', 'js', 'images', 'watch']);
